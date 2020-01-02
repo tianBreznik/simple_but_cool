03b81f8eabba15b7ec6199d2d44f214c9a23f4d6
Tian Breznik <tian.breznik@student.manchester.ac.uk>
2020-01-02 12:02:58 +0100

    wavey

--- sketch.js
+++ sketch.js
@@ -9,9 +9,12 @@ function draw() {
 	translate(width/2, height/2);
 	rectMode(CENTER);
 
+	let offset = 0; // -optional - drugace daj samo angle namesto a v sin
 	for (let x = 0; x < width;x += 10){
-		let h = map(Math.random() * sin(angle), -1, 1, 10, 100);
+    let a = angle + offset;
+		let h = map(Math.random() * sin(a), -1, 1, 10, 100);
 		rect(x,0,9,h);
+		offset += 0.1;
 	}
 
 	angle += 0.01;
